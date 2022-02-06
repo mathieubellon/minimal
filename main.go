5be1c3d4d5f0474cfa26326050f3feddee3c4f23
Matthieu <matt@pilot.pm>
2022-02-06 09:44:07 +0100

    Sample

--- main.go
+++ main.go
@@ -1,48 +1,62 @@
-// Sample run-helloworld is a minimal Cloud Run service.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START gae_go111_app]
+
+// Sample helloworld is an App Engine app.
 package main
 
+// [START import]
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-const (
-  Port = ":8080"
-)
-
-func serveStatic(w http.ResponseWriter, r *http.Request) {  
-  t, err := template.ParseFiles("index.html")
-    if err != nil {
-        fmt.Println(err)
-    }
-    items := struct {
-        Country string
-        City string
-    }{
-        Country: "France",
-        City: "Paris",
-    }
-    t.Execute(w, items)
-}
+// [END import]
+// [START main_func]
 
 func main() {
-        log.Print("starting server...")
-        http.HandleFunc("/",serveStatic)
+	http.HandleFunc("/", indexHandler)
 
-        // Determine port for HTTP service.
-        port := os.Getenv("PORT")
-        if port == "" {
-                port = "8080"
-                log.Printf("defaulting to port %s", port)
-        }
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
 
-        // Start HTTP server.
-        log.Printf("listening on port %s", port)
-        if err := http.ListenAndServe(":"+port, nil); err != nil {
-                log.Fatal(err)
-        }
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
 }
 
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// [END indexHandler]
+// [END gae_go111_app]
